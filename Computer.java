3e7e74ef8ac72c1d0a796c882c5bcafbd651f9ef
m-khan44 <m-khan44@MR00328>
2026-02-25 17:20:01 -0500

    Computer progress

--- Computer.java
+++ Computer.java
@@ -1,7 +1,10 @@
 import java.util.Random;
 
 public class Computer {    
-    // We don't typically refer to GB with fractions, so leave the RAM and hard disk space size an int
+
+    // Private variables
+
+    // We don't typically refer to GB in fractional form, so leave the RAM and hard disk space size an int
     // everything else can be a double
 
     private String manufacturerName;// Ex: IBM, Lenovo, Dell
@@ -16,12 +19,13 @@ public class Computer {
         this.RAMSize = RAMSize;
         this.hardDiskSize = hardDiskSize;
         this.powerConsumption = powerConsumption;
-
-      
     }
 
     // Mutators
-    public void overclock(double amount) {}
+    public void overclock(double amount) {
+        // Scale CPU speed by the amount
+        CPUSpeed *= amount;
+    }
     public String toString() {
         return manufacturerName;
     }
@@ -29,9 +33,10 @@ public class Computer {
     public static void main(String[] args) {
         /*
         Create a new Dell Workstation with 3.85GHz clock, 64 GB RAM, 1TB storage, and 180W power.
+        and a new Lenovo Desktop with 3.10Ghz clock, 16GB RAM, 512GB storage, and 50W power
         */
         Computer dellPC = new Computer("Dell", 3.85, 64, 1024, 180);
-
+        Computer thinkCentre = new Computer("Lenovo", 3.10, 16, 512, 50);
     }
 }
 
